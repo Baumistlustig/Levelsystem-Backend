2995ae07874831893f7c2c046102dba277309a25
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-06-23 19:47:31 +0200

    Added levenstein distance to search.leveling.service.ts;

--- src/routes/leveling/services/search.leveling.service.ts
+++ src/routes/leveling/services/search.leveling.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { find } from '../../../utils/database/database';
+import { closest, distance } from 'fastest-levenshtein';
 
 @Injectable()
 export class SearchLevelingService {
@@ -9,6 +10,25 @@ export class SearchLevelingService {
 
     if (dbResponse[0] === undefined) {
       dbResponse = await find({ minecraft_name: username }, 'users');
+    }
+
+    if (dbResponse[0] === undefined) {
+      let dbLevResponse: any[];
+      dbLevResponse = await find({}, 'users');
+
+      const dbUsernames = [];
+
+      for (let i = 0; i < dbLevResponse.length; i++) {
+        dbUsernames.push(dbLevResponse[i].discord_name);
+      }
+
+      const closestUser = closest(username, dbUsernames);
+
+      if (distance(username, closestUser) < 4) {
+        dbLevResponse = await find({ discord_name: closestUser }, 'users');
+        dbLevResponse.push('closest_result');
+        return dbLevResponse;
+      }
     }
 
     if (dbResponse[0] === undefined) {
